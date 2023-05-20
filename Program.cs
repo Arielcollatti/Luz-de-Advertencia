5e488b85e6594d8da33843095af3d628342541f2
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-20 09:05:46 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine(" --- Simulador de Luz de Advertência --- \n ");
+
+Console.Write("Digite {A}tivado,{D}esativado \n");
+
+Console.Write("Sensor de temperatura....: ");
+string tempe = Console.ReadLine()!;
+
+Console.Write("Sensor de presssão.......: ");
+string press = Console.ReadLine()!;
+
+Console.Write("Sensor de rotação........: ");
+string  rota = Console.ReadLine()!;
+
+if (tempe == "A" && (press == "A" || rota == "D"))
+{
+    Console.ForegroundColor =  ConsoleColor.Green;
+    Console.WriteLine(" Luz de advertencia estará  ativada  ");
+
+} else 
+    {
+    Console.ForegroundColor =  ConsoleColor.DarkRed;
+    Console.WriteLine(" Luz de advertencia estará  desativada ");
+    }
+Console.ResetColor();
+
+
+
+
+
